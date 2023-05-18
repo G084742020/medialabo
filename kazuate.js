ad20e1a69b3fe8052bcb449b4cfa28cb03a6dc70
G084742020 <208474@st.takushoku-u.ac.jp>
2023-05-18 13:50:45 +0900

    編集

--- kazuate.js
+++ kazuate.js
@@ -2,22 +2,22 @@ let kotae = Math.floor(Math.random()*10) + 1;
 let kaisu = 4;
 
 for(let i=0; i<kaisu; i++) {
-  let p1 = document.querySelector('span#kaisu');
-  p1.textContent = i+1;
+  let span_kaisu = document.querySelector('span#kaisu');
+  span_kaisu.textContent = i+1;
   hantei();
 }
 
 function hantei() {
   let yoso = 4;
-  let p2 = document.querySelector('span#answer');
-  p2.textContent = yoso;
+  let span_answer = document.querySelector('span#answer');
+  span_answer.textContent = yoso;
   
-  let p3 = document.querySelector('p#result');
+  let p_result = document.querySelector('p#result');
   if(kotae == yoso) {
-    p3.textContent = '正解です．おめでとう！';
+    p_result.textContent = '正解です．おめでとう！';
   } else if(kotae > yoso) {
-    p3.textContent = '間違いです．答えはもっと大きいですよ．';
+    p_result.textContent = '間違いです．答えはもっと大きいですよ．';
   } else if(kotae < yoso) {
-    p3.textContent = '間違いです．答えはもっと小さいですよ．';
+    p_result.textContent = '間違いです．答えはもっと小さいですよ．';
   }
 }
